ee8557f0ccfc18a30c54bf3c33733840eac9d26d
yan <a0726h77@gmail.com>
2014-02-08 18:41:20 +0800

    .

---
+++ co_working_space.sql
@@ -0,0 +1,10 @@
+INSERT INTO `co_working_space` (`id`, `name`, `address`, `lat`, `lng`) VALUES
+(1,	'混_hun',	'北',	NULL,	NULL),
+(2,	'創立方',	'北',	NULL,	NULL),
+(3,	'Changee',	'北',	NULL,	NULL),
+(4,	'卡市達創業加油站',	'北',	NULL,	NULL),
+(5,	'CLBC',	'北',	NULL,	NULL),
+(6,	'Hands Up 創業智庫',	'中',	NULL,	NULL),
+(7,	'好伴駐創工作室',	'中',	NULL,	NULL),
+(8,	'Dakuo 高雄市數位內容創意中心',	'南',	NULL,	NULL),
+(9,	'Cooospace',	'',	NULL,	NULL);
---
+++ group_events.sql
@@ -0,0 +1,4 @@
+INSERT INTO `group_events` (`id`, `group_id`, `name`) VALUES
+(1,	10,	'Python Taiwan Sprint!'),
+(2,	98,	'MIT Game Jam'),
+(3,	98,	'Indie Speed Run');
